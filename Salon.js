5d89ca36588e60c6eff98b1684e8a2d3dd093609
Kevin <vongkevin704@gmail.com>
2024-07-02 21:41:47 +0200

    Ajout js et css pour rendre la page Services plus dynamique

--- Salon.js
+++ Salon.js
@@ -6,6 +6,26 @@ if (logoElements.length > 0) {
     });
 }
 
-$('#coupe').on('click',function(){
-    $('#ContenuCoupes').hide();
+$(document).ready(function() {
+    // Affiche le contenu correspondant lorsque l'on clique sur les boutons
+    $('#coupes').on('click', function() {
+        $('#coupes, #couleur, #lissage').css('color', '');
+        $('#coupes').css('color', 'red');
+        $('.content-section').hide();
+        $('#ContenuCoupes').show();
+    });
+
+    $('#couleur').on('click', function() {
+        $('#coupes, #couleur, #lissage').css('color', '');
+        $('#couleur').css('color', 'red');
+        $('.content-section').hide();
+        $('#ContenuCouleur').show();
+    });
+
+    $('#lissage').on('click', function() {
+        $('#coupes, #couleur, #lissage').css('color', '');
+        $('#lissage').css('color', 'red');
+        $('.content-section').hide();
+        $('#ContenuLissage').show();
+    });
 });
